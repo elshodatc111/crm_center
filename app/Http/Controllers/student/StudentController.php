d3b1a41eb344722fb1526b26110956c63aa5d2e0
USER <elshodatc111>
2025-02-19 19:17:52 +0500

    Save

--- app/Http/Controllers/student/StudentController.php
+++ app/Http/Controllers/student/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\student;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Social;
 use App\Http\Requests\StoreVisitRequest;
 use App\Services\StudentService;
 use App\Jobs\SendMessageWork;

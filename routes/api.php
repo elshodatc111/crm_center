6e9a4d9960d5e914392b114f16c8a039f59aa222
USER <elshodatc111>
2025-03-04 12:14:30 +0500

    Save

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\techer\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/techer/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::post('/techer/logout', [AuthController::class, 'logout']);
+});

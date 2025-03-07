f7aa955acd11692577e70d5294d18423e81ec462
USER <elshodatc111>
2025-03-07 11:00:48 +0500

    Save

--- app/Http/Controllers/api/user/TestUserController.php
+++ app/Http/Controllers/api/user/TestUserController.php
@@ -4,12 +4,56 @@ namespace App\Http\Controllers\api\user;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\GroupUser;
+use App\Models\Group;
+use App\Models\Cours;
+use App\Models\CoursVideo;
+use App\Models\GroupDays;
+use App\Models\CoursTest;
+use App\Models\TestCheck;
 use Illuminate\Support\Facades\Log;
 
-class TestUserController extends Controller
-{
+class TestUserController extends Controller{
     public function index(){
-        return response()->json(['testlar' => []], 200);
+        $now = date('Y-m-d H:i:s');
+        $GroupUser = GroupUser::where('group_users.user_id', auth()->user()->id)
+            ->where('group_users.status', 1)
+            ->where('groups.lessen_end', '<=', $now)
+            ->join('groups', 'group_users.group_id', '=', 'groups.id')
+            ->select('groups.cours_id', 'groups.id as group_id', 'groups.group_name')
+            ->get();
+        $Testlar = [];
+        foreach ($GroupUser as $key => $value) {
+            $TestCheck = TestCheck::where('user_id', auth()->user()->id)->where('group_id', $value['group_id'])->first();
+            $CoursTest = CoursTest::where('cours_id', $value['cours_id'])->inRandomOrder()->limit(15)->get();
+            $Test = [];
+            foreach ($CoursTest as $key2 => $value2) {
+                $answers = [
+                    ["test" => $value2->javob_true, "status" => true],
+                    ["test" => $value2->javob_false_first, "status" => false],
+                    ["test" => $value2->javob_false_two, "status" => false],
+                    ["test" => $value2->javob_false_thre, "status" => false],
+                ];
+                shuffle($answers);
+                $Test[$key2] = [
+                    'test' => $value2->test,
+                    'javob' => $answers
+                ];
+            }
+            $Testlar[$key] = [
+                'cours_id' => $value['cours_id'],
+                'group_id' => $value['group_id'],
+                'user_id' => auth()->user()->id,
+                'group_name' => $value['group_name'],
+                'testlar' => 15,
+                'urinishlar' => $TestCheck ? $TestCheck->count : 0,
+                'tugri_javob' => $TestCheck ? $TestCheck->count_true : 0,
+                'ball' => $TestCheck ? ($TestCheck->count_true * 2) : 0,
+                'testlar' => $Test,
+            ];
+        }
+        return response()->json(['testlar' => $Testlar], 200);
     }
 
     public function shows($id){
--- app/Http/Controllers/api/user/VideoUserController.php
+++ app/Http/Controllers/api/user/VideoUserController.php
@@ -4,14 +4,39 @@ namespace App\Http\Controllers\api\user;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\GroupUser;
+use App\Models\Group;
+use App\Models\Cours;
+use App\Models\CoursVideo;
+use App\Models\GroupDays;
+use App\Models\LessenTime;
+use Illuminate\Support\Facades\Log;
 
-class VideoUserController extends Controller
-{
+class VideoUserController extends Controller{
     public function index(){
-        return response()->json(['video' => []], 200);
+        $now = date('Y-m-d H:i:s');
+        $GroupUser = GroupUser::where('group_users.user_id', auth()->user()->id)
+            ->where('group_users.status', 1)
+            ->where('groups.lessen_end', '>=', $now)
+            ->join('groups', 'group_users.group_id', '=', 'groups.id')
+            ->select('groups.cours_id')
+            ->distinct()
+            ->get();
+        $Cours = [];
+        foreach ($GroupUser as $key => $value) {
+            $Cours[$key]['id'] = $value['cours_id'];
+            $Cours[$key]['name'] = Cours::find($value['cours_id'])->cours_name;
+        }
+        return response()->json(['video' => $Cours], 200);
     }
 
     public function shows($id){
-        return response()->json(['video_about' => $id], 200);
+        $Cours = Cours::find($id);
+        $Video = CoursVideo::where('cours_id',$id)->orderby('lessen_number','asc')->select('id','cours_name','video_url')->get();
+        return response()->json(['cours' => [
+            'id' => $Cours['id'],
+            'cours_name' => $Cours['cours_name'],
+        ],'video' => $Video], 200);
     }
 }

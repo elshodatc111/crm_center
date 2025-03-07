3aeb64582de3fee4ffb77b3e43d9283a02b4657d
elshodatc111 <elshodatc1116@gmail.com>
2025-03-07 22:58:25 +0500

    Save

--- app/Http/Controllers/api/techer/GroupsTecherController.php
+++ app/Http/Controllers/api/techer/GroupsTecherController.php
@@ -5,52 +5,54 @@ namespace App\Http\Controllers\api\techer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Group;
+use App\Models\TestCheck;
+use App\Models\TecherPaymart;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use App\Http\Requests\DavomadPostRequest;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+use App\Services\techer\TecherAppService;
 
-class AuthController extends Controller{
-    
-    public function login(Request $request){
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-        $user = User::where('email', $request->email)->where('type','techer')->first();
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+class GroupsTecherController extends Controller{
+
+    protected $techerAppService;
+
+    public function __construct(TecherAppService $techerAppService){
+        $this->techerAppService = $techerAppService;
+    } 
+
+    public function groups(){
+        $res = $this->techerAppService->allGroups();
+        return response()->json($res, 200);
+    }
+
+    public function group($id){
+        $res = $this->techerAppService->group($id);
+        return response()->json($res, 200);
+    }
+
+    public function davomadPost(DavomadPostRequest $request){
+        foreach ($request->attendances as $attendanceData) {
+            $check = $this->techerAppService->checkDavomadDay($attendanceData);
+            if($check){
+                $res = $this->techerAppService->CreateDavomad($attendanceData);
+                return response()->json(['message' => 'Davomad olindi'], 201);
+            }else{
+                return response()->json(['message' => 'Bugun davomad kuni emas'], 422);
+            }
         }
-        $token = $user->createToken('auth-token')->plainTextToken;
-        return response()->json(['token' => $token], 200);
     }
 
-    public function logout(Request $request){
-        $request->user()->tokens()->delete();
-        return response()->json(['message' => 'Logged out'], 200);
+    public function paymarts(){
+        $TecherPaymart = TecherPaymart::where('user_id',auth()->user()->id)->get();
+        return response()->json(['message' => $TecherPaymart], 422);
     }
-
-    public function profile(){
-        return response()->json(auth()->user(), 200);
-    }
-
-    public function changePassword(Request $request){
-        $request->validate([
-            'current_password' => 'required',
-            'new_password' => 'required|min:6|confirmed',
-        ]);
-        $user = $request->user();
-        if (!Hash::check($request->current_password, $user->password)) {
-            throw ValidationException::withMessages([
-                'current_password' => ['Joriy parol noto‘g‘ri!']
-            ]);
-        }
-        $user->password = Hash::make($request->new_password);
-        $user->save();
-        return response()->json(['message' => 'Parol muvaffaqiyatli yangilandi!'], 200);
-    }
-
-
-
 
 }
+
+
+
+
+

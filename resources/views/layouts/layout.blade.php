04dc214c8ca0c4529243665d1d3a3bca7752fa60
elshodatc111 <elshodatc1116@gmail.com>
2025-03-09 00:02:01 +0500

    Save

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -263,7 +263,7 @@
       &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+    <strong><span>CodeStart</span></strong> Dev Center
     </div>
   </footer>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>

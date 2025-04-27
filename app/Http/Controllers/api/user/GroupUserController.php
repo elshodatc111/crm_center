82abfbfd7f712bb6bb373ea1b0a39b22fed5e2fd
elshodatc111 <elshodatc1116@gmail.com>
2025-04-27 13:30:41 +0500

    Save

--- app/Http/Controllers/api/user/GroupUserController.php
+++ app/Http/Controllers/api/user/GroupUserController.php
@@ -8,7 +8,9 @@ use App\Models\User;
 use App\Models\GroupUser;
 use App\Models\Group;
 use App\Models\CoursAudio;
+use App\Models\SettingRoom;
 use App\Models\GroupDays;
+use App\Models\CoursVideo;
 use App\Models\LessenTime;
 use Illuminate\Support\Facades\Log;
 
@@ -27,16 +29,26 @@ class GroupUserController extends Controller
         return response()->json(['groups' => $GroupUser], 200);
     }
     public function show($id){
-        $Group = Group::where('groups.id',$id)
-            ->join('cours','groups.cours_id','cours.id')
-            ->join('lessen_times','groups.cours_id','lessen_times.id')
-            ->join('users','groups.techer_id','users.id')
-            ->join('setting_rooms','groups.setting_rooms_id','setting_rooms.id')
-            ->select('groups.id','groups.cours_id','groups.group_name','groups.price','groups.lessen_count','groups.lessen_start','groups.lessen_end','cours.cours_name','users.user_name as techer','setting_rooms.room_name','lessen_times.time')
-            ->first();
+        $Group = Group::where('groups.id',$id)->first();
+        $Groups = [
+            'id' => $Group->id,
+            'group_name' => $Group->group_name,
+            'price' => $Group->price,
+            'lessen_count' => $Group->lessen_count,
+            'lessen_start' => $Group->lessen_start,
+            'lessen_end' => $Group->lessen_end,
+            'cours_name' => $Group->cours_name,
+            'techer' => User::find($Group->techer_id)->user_name,
+            'room_name' => SettingRoom::find($Group->setting_rooms_id)->room_name,
+            'time' => $Group->time,
+            'cours_id' => $Group->cours_id,
+        ];
         $days = GroupDays::where('group_id',$id)->select('date')->get();
-        $audios = CoursAudio::where('cours_id',$Group->cours_id)->select('audio_name','audio_number','audio_url')->orderby('audio_number','asc')->get();
-        return response()->json(['groups' => $Group,'days'=>$days,'audios'=>$audios], 200);
+        $audios = CoursAudio::where('cours_id',$Groups['cours_id'])->select('audio_name','audio_number','audio_url')->orderby('audio_number','asc')->get();
+        $audio_status = count($audios) > 0 ? 1 : 0;
+        $Video = CoursVideo::where('cours_id',$Groups['cours_id'])->orderby('lessen_number','asc')->select('id','cours_name','video_url')->get();
+        $video_status = count($Video) > 0 ? 1 : 0;
+        return response()->json(['groups' => $Group,'days'=>$days,'audio_status'=>$audio_status,'audios' => $audios,'video_status'=>$video_status,'video' => $Video], 200);
     }
     
 }
